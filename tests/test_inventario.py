b0c6a4d51b6d088ef3a28262c61eea9706b6c358
Andy Macas <andypj004@gmail.com>
2025-12-27 15:27:47 -0500

    Agregar test para recepción de mercancía de inventario

--- tests/test_inventario.py
+++ tests/test_inventario.py
@@ -1,3 +1,18 @@
 def test_recepcion_mercancia(client):
     """ Test de la recepción de mercancía en el inventario. """
 
+    client.post(
+        "/catalogo/crear",
+        json={
+            "id": "P100",
+            "nombre": "Aceite",
+            "codigoBarras": "555555",
+            "precio": 3.0,
+            "categoria": "Abarrotes",
+            "stock": 5
+        }
+    )
+
+    response = client.post("/inventario/recepcion/P100?cantidad=5")
+    assert response.status_code == 200
+
